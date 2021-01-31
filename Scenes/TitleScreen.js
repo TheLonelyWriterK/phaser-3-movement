ce3495be3ec2aa16058c561d79ef5bbb9c2e1cad
Kaleb Brown <brown.kaleb2001@gmail.com>
2021-01-31 16:12:31 -0600

    Test

--- Scenes/TitleScreen.js
+++ Scenes/TitleScreen.js
@@ -3,22 +3,29 @@ class TitleScreen extends Phaser.Scene
     constructor()
     {
         super("TitleScreen")
+        this.leftButton = null;
+        this.recA = null;
     }
 
     preload()
     {
-
+        
     }
 
     create()
     {
         console.log("Title Screen")
-        this.add.rectangle(0,0,100,100,0xffffff)
+        this.recA = this.add.rectangle(50,50,100,100,0xffffff)
+
+        this.leftButton = this.input.keyboard.addKey("A");
     }
 
     update()
     {
-
+        if(this.leftButton.isDown)
+        {
+            this.recA.x -= 10;
+        }
     }
 
     rect(x, y, width, height, color)
