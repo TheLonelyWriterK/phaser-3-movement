96f74b572c899b43fef00874c944ffca45532adc
Kaleb Brown <brown.kaleb2001@gmail.com>
2021-01-31 18:05:18 -0600

    Test

--- Scenes/Scene1.js
+++ Scenes/Scene1.js
@@ -2,21 +2,68 @@ class Scene1 extends Phaser.Scene
 {
     constructor()
     {
+        //Name Of Scene
         super("Scene1")
+
+        //Variables
+        this.recA = null;
     }
+
     preload()
     {
-
+        
     }
 
     create()
     {
-        console.log("Scene 1")
+        // logs for scene 1
+        console.log("Scene1")
+
+        // Creates a rectangle on scene 1
+        this.recA = this.add.rectangle(50,50,100,100,0xffffff)
+
+        // creates a listener for an A key
+        this.leftButton = this.input.keyboard.addKey("A");
+        // creates a listener for an D key
+        this.rightButton = this.input.keyboard.addKey("D");
+        // creates a listener for an W key
+        this.upButton = this.input.keyboard.addKey("W");
+        // creates a listener for an S key
+        this.downButton = this.input.keyboard.addKey("S");
     }
 
     update()
     {
+        // move left
+        if(this.leftButton.isDown)
+        {
+            this.recA.x -= 10;
+        }
 
+        // move right 
+        if(this.rightButton.isDown)
+        {
+            this.recA.x += 10;
+        }
+
+        // move down
+        if(this.downButton.isDown)
+        {
+            this.recA.y += 10;
+        }
+
+        // move up
+        if(this.upButton.isDown)
+        {
+            this.recA.y -= 10;
+        }
+    }
+
+    // alias to create rectangle
+    rect(x, y, width, height, color)
+    {
+        var r = this.add.rectangle(x, y, width, height, color);
+        return r;
     }
 
 }
--- Scenes/TitleScreen.js
+++ Scenes/TitleScreen.js
@@ -3,58 +3,38 @@ class TitleScreen extends Phaser.Scene
     constructor()
     {
         super("TitleScreen")
-        this.leftButton = null;
+
         this.recA = null;
     }
-
     preload()
     {
-        
+
     }
 
     create()
     {
         console.log("Title Screen")
-        this.recA = this.add.rectangle(50,50,100,100,0xffffff)
+        this.recA = this.add.rectangle(800,450,50,50,0xffffff)
+        // this.recA.setInteractive()
+        // RecA.on("pointerdown",this.onClick,this)
 
-        this.leftButton = this.input.keyboard.addKey("A");
-        this.rightButton = this.input.keyboard.addKey("D");
-        this.upButton = this.input.keyboard.addKey("W");
-        this.downButton = this.input.keyboard.addKey("S");
     }
 
     update()
     {
-        if(this.leftButton.isDown)
-        {
-            this.recA.x -= 10;
-        }
 
-        if(this.rightButton.isDown)
-        {
-            this.recA.x += 10;
-        }
-
-        if(this.downButton.isDown)
-        {
-            this.recA.y += 10;
-        }
-
-        if(this.upButton.isDown)
-        {
-            this.recA.y -= 10;
-        }
     }
 
+    // alias to create rectangle
     rect(x, y, width, height, color)
     {
         var r = this.add.rectangle(x, y, width, height, color);
         return r;
     }
 
-    move()
+    onClick()
     {
-
+        this.scene.start("Scene1")
     }
 
 }
